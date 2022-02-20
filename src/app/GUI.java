a01de57bff1238f813e7f2e585bb88124356412e
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-02-19 23:44:36 -0500

    WIP: designed UI for add character interfaces

--- src/app/GUI.java
+++ src/app/GUI.java
@@ -5,13 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.awt.CardLayout;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class GUI {
 
 	private JFrame frame;
-	private JPanel panel_add;
+	private JPanel panel_addStudent;
 	private JPanel panel_edit;
 	private JPanel panel_view;
+	private JLabel lbl_schoolName;
+	private JLabel lbl_grade;
 
 	/**
 	 * Launch the application.
@@ -46,9 +49,33 @@ public class GUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new CardLayout(0, 0));
 		
-		panel_add = new JPanel();
-		frame.getContentPane().add(panel_add, "panel_add");
-		panel_add.setLayout(null);
+		panel_addStudent = new JPanel();
+		frame.getContentPane().add(panel_addStudent, "panel_addStudent");
+		panel_addStudent.setLayout(null);
+		
+		JLabel lbl_title = new JLabel("Create Student");
+		lbl_title.setBounds(174, 12, 107, 17);
+		panel_addStudent.add(lbl_title);
+		
+		JLabel lbl_firstName = new JLabel("First Name:");
+		lbl_firstName.setBounds(12, 35, 107, 17);
+		panel_addStudent.add(lbl_firstName);
+		
+		JLabel lbl_lastName = new JLabel("Last Name:");
+		lbl_lastName.setBounds(12, 70, 107, 17);
+		panel_addStudent.add(lbl_lastName);
+		
+		JLabel lbl_age = new JLabel("Age:");
+		lbl_age.setBounds(12, 109, 107, 17);
+		panel_addStudent.add(lbl_age);
+		
+		lbl_schoolName = new JLabel("School Name");
+		lbl_schoolName.setBounds(12, 148, 107, 17);
+		panel_addStudent.add(lbl_schoolName);
+		
+		lbl_grade = new JLabel("Grade:");
+		lbl_grade.setBounds(12, 187, 107, 17);
+		panel_addStudent.add(lbl_grade);
 		
 		panel_edit = new JPanel();
 		frame.getContentPane().add(panel_edit, "panel_edit");
@@ -58,5 +85,4 @@ public class GUI {
 		frame.getContentPane().add(panel_view, "panel_view");
 		panel_view.setLayout(null);
 	}
-
 }
