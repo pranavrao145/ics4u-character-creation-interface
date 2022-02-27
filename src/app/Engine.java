b9f9eb02b2499b348e99ae5e725e6fbdb623d8c0
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-02-27 18:42:40 -0500

    feat(database): added write to text file functionality

--- src/app/Engine.java
+++ src/app/Engine.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import models.*;
 
@@ -15,6 +18,66 @@ public class Engine {
     teachers = new ArrayList<Teacher>();
     internationalStudents = new ArrayList<InternationalStudent>();
     gui = currentGUI;
+  }
+
+  public static void saveDataToFile() {
+    try {
+      // create the new file if it doesn't exist
+      File studentFile = new File("data/student_logs.txt"),
+           teacherFile = new File("data/teacher_logs.txt"),
+           internationalStudentFile =
+               new File("data/international_student_logs.txt");
+
+      studentFile.delete();
+      teacherFile.delete();
+      internationalStudentFile.delete();
+
+      studentFile.getParentFile().mkdirs();
+      studentFile.createNewFile();
+
+      teacherFile.getParentFile().mkdirs();
+      teacherFile.createNewFile();
+
+      internationalStudentFile.getParentFile().mkdirs();
+      internationalStudentFile.createNewFile();
+
+      PrintWriter studentWriter =
+          new PrintWriter(new FileWriter(studentFile, true));
+      PrintWriter teacherWriter =
+          new PrintWriter(new FileWriter(teacherFile, true));
+      PrintWriter internationalStudentWriter =
+          new PrintWriter(new FileWriter(internationalStudentFile, true));
+
+      for (Student student : students) {
+        String content =
+            String.format("%s, %d", student.getName(), student.getGrade());
+
+        studentWriter.println(content);
+      }
+
+      for (Teacher teacher : teachers) {
+        String content =
+            String.format("%s, %s, %s", teacher.getName(),
+                          teacher.getSubject1(), teacher.getSubject2());
+
+        teacherWriter.println(content);
+      }
+
+      for (InternationalStudent internationalStudent : internationalStudents) {
+        String content = String.format(
+            "%s, %d, %s", internationalStudent.getName(),
+            internationalStudent.getGrade(), internationalStudent.getCountry());
+
+        internationalStudentWriter.println(content);
+      }
+
+      studentWriter.close();
+      teacherWriter.close();
+      internationalStudentWriter.close();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public static void run(GUI currentGUI) { initialize(currentGUI); }
--- src/app/GUI.java
+++ src/app/GUI.java
@@ -4,6 +4,8 @@ import java.awt.CardLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -840,6 +842,16 @@ public class GUI {
             Engine.getInternationalStudents().get(index));
         updateViewTable(InternationalStudent.class, table_viewIntlStudents);
         masterLayout.show(contentPane, "panel_viewIntlStudents");
+      }
+    });
+
+    // listener for when the window closes
+    frame.addWindowListener(new WindowAdapter() {
+      @Override
+      public void windowClosing(WindowEvent e) {
+        Engine.saveDataToFile();
+        frame.dispose();
+        System.exit(0);
       }
     });
   }
