4e6d2dc99b5756e6851bc345e40ccb6380c8c0b9
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2022-03-01 19:41:20 -0500

    feat(comments): added comments to App class

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,26 @@
-package app;
+/******************************************************************************
+Program: App Class (Character Creation Interface)
+
+Description: This is the driver code for this character creation interface.
+This class' main method will start the entire program.
+
+Author: Pranav Rao
+
+Date: March 1, 2022
+*******************************************************************************/
+
+package app; // declare this class as part of the app package
 
 import java.awt.EventQueue;
 
 public class App {
 
+  /**
+   * This function is the driver code for the entire program. It creates a new
+   * GUI and runs the engine.
+   *
+   * @param args
+   */
   public static void main(final String[] args) {
     EventQueue.invokeLater(new Runnable() {
       public void run() {
